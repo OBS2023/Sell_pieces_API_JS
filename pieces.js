2bbb8cc5720b446435b89cd2d285d48d78678f17
OBS2023 <o.b.salomon01@gmail.com>
2023-08-29 12:40:23 +0200

    trier et filtrer les pieces par ordre croissant et decroissant...

--- pieces.js
+++ pieces.js
@@ -79,3 +79,47 @@ for (let i = 0; i < pieces.length; i++) {
     pieceElement.appendChild(disponibiliteElement);
 }
 
+//fonction de trie par ordre croissant avec la méthode Sort
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+
+});
+//pour n’afficher que les pièces dont le prix est inférieur ou égal à 35 €.
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+    console.log(piecesFiltrees);
+});
+
+//Trier par ordre decroissant
+const boutonTrierdecro = document.querySelector('.btn-trier-decro');
+boutonTrierdecro.addEventListener('click', function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+
+/*const truc = pieces[3].description.length;
+console.log(truc);*/
+
+
+//afficher que celle qui ont des descriptions (btn-filtrer-desc)
+const boutonFiltrerDesc = document.querySelector('.btn-filtrer-desc');
+boutonFiltrerDesc.addEventListener('click',function() {
+         const piecesFiltrees = pieces.filter(function(piece) {
+            return piece.description;
+         });
+         console.log(piecesFiltrees);
+    }); 
+
+
