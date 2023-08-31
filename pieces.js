66556bf4a25c05b158cb4d2e206426ee08c68f55
OBS2023 <o.b.salomon01@gmail.com>
2023-08-31 15:47:23 +0200

    Generation automatique de la page et mise à jour apres filtrage et triage.

--- pieces.js
+++ pieces.js
@@ -1,4 +1,4 @@
-// const respose = await fetch("pieces-autos.json");
+/*// const respose = await fetch("pieces-autos.json");
 // const pieces = await respose.json();
 // //creation des elements
 // const article = pieces[0];
@@ -46,6 +46,7 @@ sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
 sectionFiches.appendChild(descriptionElement);
 sectionFiches.appendChild(disponibiliteElement);*/
+/*
 for (let i = 0; i < pieces.length; i++) {
     // Récupération de l'élément du DOM qui accueillera les fiches
     const sectionFiches = document.querySelector(".fiches");
@@ -112,22 +113,121 @@ boutonTrierdecro.addEventListener('click', function () {
 /*const truc = pieces[3].description.length;
 console.log(truc);*/
 
-
+/*
 //afficher que celle qui ont des descriptions (btn-filtrer-desc)
 const boutonFiltrerDesc = document.querySelector('.btn-filtrer-desc');
-boutonFiltrerDesc.addEventListener('click',function() {
-         const piecesFiltrees = pieces.filter(function(piece) {
-            return piece.description;
-         });
-         console.log(piecesFiltrees);
-    }); 
+boutonFiltrerDesc.addEventListener('click', function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description;
+    });
+    console.log(piecesFiltrees);
+});
 //Parcourir la liste des pièces avec une boucle for.
 //Supprimer les noms avec la fonction splice.
-const noms = pieces.map((piece) =>  piece.nom);
-   
-for (let i =  pieces.length -1;i >=0; i--){
-    if(pieces[i].prix >35){
+const noms = pieces.map((piece) => piece.nom);
+
+for (let i = pieces.length - 1; i >= 0; i--) {
+    if (pieces[i].prix > 35) {
         noms.splice(i, 1);
     }
 }
-//af
+//affichez la liste des pieces abordables
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for (let i = 0; i < noms.length; i++) {
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(abordablesElements)
+
+
+//affichier les pieces disponibles
+let prixDisponibles = pieces.map((piece) => piece.prix); 
+let nomDisponibles = pieces.map((piece) => piece.nom);
+for(let i = pieces.length - 1; i >= 0; i--){
+    
+    if(pieces[i].disponibilite ===false){
+        prixDisponibles.splice(i, 1);
+        nomDisponibles.splice(i, 1);
+    }
+}
+const disponiblesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+//for (let i = 0; i < pieces.length; i++) {
+for (let i = 0; i < nomDisponibles.length; i++) {
+    const nomElement = document.createElement('li');
+    
+    nomElement.innerText = `${nomDisponibles[i]}  -  ${prixDisponibles[i]} € `; 
+        
+    disponiblesElements.appendChild(nomElement)
+}
+
+
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.disponibles')
+    .appendChild(disponiblesElements*/
+
+const pieces = await fetch('pieces-autos.json').then(pieces => pieces.json());
+
+// Fonction qui génère toute la page web
+function genererPieces(pieces) {
+    for (let i = 0; i < pieces.length; i++) {
+        const pieceElelment = document.createElement('article');
+        const imageElement = document.createElement('img');
+        const nomElement = document.createElement("h2");
+        const prixElement = document.createElement("p");
+        const categorieElement = document.createElement("p");
+        const descriptionElement = document.createElement("p");
+        const disponibiliteElement = document.createElement("p");
+
+        imageElement.src = pieces[i].image;
+        nomElement.innerHTML = pieces[i].nom;
+        prixElement.innerHTML = pieces[i].prix;
+        categorieElement.innerHTML = pieces[i].categorie;
+        descriptionElement.innerHTML = pieces[i].description;
+        disponibiliteElement.innerHTML = pieces[i].disponibilite;
+
+        pieceElelment.appendChild(imageElement);
+        pieceElelment.appendChild(nomElement);
+        pieceElelment.appendChild(prixElement);
+        pieceElelment.appendChild(categorieElement);
+        pieceElelment.appendChild(descriptionElement);
+        pieceElelment.appendChild(disponibiliteElement);
+
+        const sectionFiches = document.querySelector(".fiches");
+        sectionFiches.appendChild(pieceElelment);
+    }
+}
+
+genererPieces(pieces);
+
+
+// Ajout du listener pour trier les pièces par ordre de prix croissant
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+    })
+
+    // Effacement de l'écran et regénération de la page
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
+});
+
+//ajout du listener pour filtrer les pieces non disponible.
+const boutonFilter = document.querySelector(".btn-filter");
+boutonFilter.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.disponibles;
+    })
+    // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
+});
+
